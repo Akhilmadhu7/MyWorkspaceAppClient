f1448f4a63cdb9047f6f88c12d3d4b565fecf6ea
akhil <akhil.madhu@peerxp.com>
2026-02-26 08:33:00 +0530

    updated

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound({message}) {
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-400">404</p>
-          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-[#071089] sm:text-7xl">
             Page not found
           </h1>
           <p className="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
@@ -14,12 +14,9 @@ export default function NotFound({message}) {
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="#"
-              className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              className="rounded-md bg-[#0d17a9] px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-[#060d6a]/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#060d6a]"
             >
               Go back home
-            </a>
-            <a href="#" className="text-sm font-semibold text-white">
-              Contact support <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </div>
